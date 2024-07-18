fbe232224cf2b319788e9085e1519a780eefd923
sudo-odner <odner.main@gmail.com>
2024-07-18 10:37:04 +0300

    Setting config and write config file

--- config/config.go
+++ config/config.go
@@ -1,9 +1,57 @@
 package config
 
+import (
+	"errors"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
 type Config struct {
+	Logger     string     `yaml:"logger" env-default:"info"` // Прочитаь про struct tag
+	HTTPServer HTTPServer `yaml:"http_server"`
+	DB         DB         `yaml:"db"`
+}
+
+type HTTPServer struct {
+	Host string `yaml:"host" env-default:"localhost"`
+	Port string `yaml:"port" env-default:"8080"`
+}
+
+type DB struct {
+	RebuildMode bool    `yaml:"rebuild_mode"`
+	MongoDB     MongoDB `yaml:"mongo_db"`
+}
+
+type MongoDB struct {
+	Uri string `yaml:"uri"`
 }
 
 func New() (*Config, error) {
-	// TODO: write config
-	return &Config{}, nil
+	// Провекрка существования .env
+	if err := godotenv.Load(); err != nil {
+		log.Print(".env file found")
+		return nil, err
+	}
+
+	configPath, exists := os.LookupEnv("CONFIG_PATH")
+	if !exists {
+		log.Fatal("CONFIG_PATH is not found")
+		return nil, errors.New("CONFIG_PATH is not found")
+	}
+
+	// Провека на существования файла
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+		return nil, err
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+		return nil, err
+	}
+
+	return &cfg, nil
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -4,10 +4,12 @@ import (
 	"github.com/sudo-odner/LifeManagerBack/config"
 	"github.com/sudo-odner/LifeManagerBack/internal/controller/http/httpHandler"
 	"github.com/sudo-odner/LifeManagerBack/internal/controller/http/httpServer"
+	"github.com/sudo-odner/LifeManagerBack/internal/logger"
 )
 
 func Run(cfg *config.Config) {
 	// TODO: Write logging
+	log := logger.New(cfg.Logger)
 	// TODO: Write repository
 	// TODO: Write usecase
 	// Init HTTP Server - controller
